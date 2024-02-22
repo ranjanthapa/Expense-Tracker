d1c9c0c50ef67224a2d657b6a04eaa931b3a981e
Ranjan Thapa <98253643+sntiago1011@users.noreply.github.com>
2024-02-22 23:10:19 +0545

    refactor code

--- expense_tracker/user/UserManager.py
+++ expense_tracker/user/UserManager.py
@@ -5,7 +5,7 @@ class UserManager:
     def __init__(self, mysql: MySQL):
         self.mysql = mysql
 
-    def login(self, email: str, password: str):
+    def login(self, email: str, password: str) -> tuple:
         cursor = self.mysql.connection.cursor()
         select_query = "SELECT email, password FROM users WHERE email = %s AND password = %s"
         cursor.execute(select_query, (email, password))
@@ -13,7 +13,7 @@ class UserManager:
         cursor.close()
         return result
 
-    def register_user(self, email: str, password: str, phone_number: str):
+    def register_user(self, email: str, password: str, phone_number: str) -> None:
         cursor = self.mysql.connection.cursor()
         insert_query = "INSERT INTO users (email, password, phone_number) VALUES (%s, %s, %s)"
         cursor.execute(insert_query, (email, password, phone_number))
