d3d64e15fec4e87294b15e5644f66edb985928cb
Ranjan Thapa <98253643+sntiago1011@users.noreply.github.com>
2024-02-26 15:04:32 +0545

    config .dotenv

--- config.py
+++ config.py
@@ -1,10 +1,14 @@
 from flask_mysqldb import MySQL
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 
 class DatabaseConfig:
     MYSQL_HOST = 'localhost'
     MYSQL_USER = 'root'
-    MYSQL_PASSWORD = '@dmin123'
+    MYSQL_PASSWORD = os.getenv("MYSQL_PASSWORD")
     MYSQL_DB = 'expense_tracker'
 
 
